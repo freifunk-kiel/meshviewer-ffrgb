emuDrache/meshviewer

[TASK] Add multiple communities map

lib/about.js
@@ -15,8 +15,24 @@ define(function () {
         '<span class="legend-24ghz"><span class="symbol"></span> 2.4 Ghz</span>' +
         '<span class="legend-5ghz"><span class="symbol"></span> 5 Ghz</span>' +
         '<span class="legend-others"><span class="symbol"></span> ' + _.t('others') + '</span>' +
-        '</p>' +
-        '<h3>AGPL 3</h3>' +
+        '</p>';
+
+      var community = '<h2>Communities</h2><ul>';
+      config.siteNames.forEach(function (item) {
+        if (item.web) {
+          community += '<li><strong>' + item.name + '</strong> ' +
+            '<a href="' + item.web + '" target="_blank">Web</a>';
+
+          if (item.map) {
+            community += ' | <a href="' + item.map + '" target="_blank">Map</a>';
+          }
+
+          community += '</li>';
+        }
+      });
+      d.innerHTML += community + '</ul>';
+
+      d.innerHTML += '<h3>AGPL 3</h3>' +
 
         '<p>Copyright (C) Milan Pässler</p>' +
         '<p>Copyright (C) Nils Schneider</p>' +

freifunk-kiel/meshviewer-ffrgb

lib/about.js
@@ -15,8 +15,24 @@ define(function () {
         '<span class="legend-24ghz"><span class="symbol"></span> 2.4 GHz</span>' +
         '<span class="legend-5ghz"><span class="symbol"></span> 5 GHz</span>' +
         '<span class="legend-others"><span class="symbol"></span> ' + _.t('others') + '</span>' +
-        '</p>' +
-        '<h3>AGPL 3</h3>' +
+        '</p>';
+
+      var community = '<h2>Communities</h2><ul>';
+      config.domainNames.forEach(function (item) {
+        if (item.web) {
+          community += '<li><strong>' + item.name + '</strong> ' +
+            '<a href="' + item.web + '" target="_blank">Web</a>';
+
+          if (item.map) {
+            community += ' | <a href="' + item.map + '" target="_blank">Map</a>';
+          }
+
+          community += '</li>';
+        }
+      });
+      d.innerHTML += community + '</ul>';
+
+      d.innerHTML += '<h3>AGPL 3</h3>' +
 
         '<p>Copyright (C) Milan Pässler</p>' +
         '<p>Copyright (C) Nils Schneider</p>' +